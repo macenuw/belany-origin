3bfb107fc6f0bcf21689eb179223cd4ea32cba9a
Amidamaru <gnykabiny54@gmail.com>
2022-09-21 20:55:16 +0300

    add search to header, add underline for privacy

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,20 @@ window.addEventListener("DOMContentLoaded", () => {
 })
 
 const menuBtn = document.querySelector('.header__btn');
-console.log(menuBtn);
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('active')
+})
+
+
+const searchBtn = document.querySelector('#searchBtn');
+const searchClose = document.querySelector('#searchClose');
+const search = document.querySelector('.search')
+
+searchBtn.addEventListener('click', () => {
+  searchBtn.classList.toggle('active');
+  search.classList.toggle('active');
+})
+searchClose.addEventListener('click', () => {
+  searchBtn.classList.toggle('active');
+  search.classList.toggle('active');
 })
